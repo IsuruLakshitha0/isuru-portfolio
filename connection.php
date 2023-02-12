7097680107a6e96dbade682cf02d797bbb3b0de9
Isuru lakshitha <123106451+IsuruLakshitha0@users.noreply.github.com>
2023-02-12 13:42:22 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,20 +1,35 @@
-<?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+	<?php
+  // Connect to MySQL database
+  $servername = "localhost";
+  $username = "id20172692_isuru";
+  $password = "Lakshitha@0820";
+  $dbname = "id20172692_portfolio";
 
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Database connection
-$conn = new mysqli('localhost', 'root', '', 'portfolio');
-if ($conn->connect_error) {
-    echo "$conn->connect_error";
-    die("Connection Failed : " . $conn->connect_error);
-} else {
-    $stmt = $conn->prepare("insert into contact(name,email,phone,message) values(?,?,?,?)");
-    $stmt->bind_param("siss", $name, $email, $phone, $message);
-    $execval = $stmt->execute();
-    $stmt->close();
-    $conn->close();
-}
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  // Get form data
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $phone = $_POST["phone"];
+  $message = $_POST["message"];
+
+  // Prepare and bind
+  $stmt = $conn->prepare("INSERT INTO contact (name, email, phone, message) VALUES (?, ?, ?, ?)");
+  $stmt->bind_param("ssss", $name, $email, $phone, $message);
+
+  // Insert data into MySQL table
+  if ($stmt->execute() === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $stmt->error;
+  }
+  $stmt->close();
+  $conn->close();
 ?>
